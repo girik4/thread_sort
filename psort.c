0e817d71da92f0ef31abde3676f95e8517b89167
Gulinazi Julati <gulinazi@snares-09.cs.wisc.edu>
2023-04-11 12:46:18 -0500

    don't know if it works yet

--- psort.c
+++ psort.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
-#include<sys/stat.h>
+#include <sys/stat.h>
 #include <sys/mman.h>
 #include <string.h>
- #include <sys/sysinfo.h>
+#include <sys/sysinfo.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #define KEY_S 4
 #define RECORD_S 96
@@ -12,78 +14,161 @@
 
 int num_records; // number of record
 struct record_key *array;
+int remain_work;
+int num_processes;
 
 struct record_key
 {
     char key[KEY_S];
     char record[RECORD_S];
-} ;
+};
 
-void merge(struct record_key *array, int left, int mid, int right){
+void merge(struct record_key *array, int left, int mid, int right)
+{
     int lsize = mid - left + 1;
     int rsize = right - mid;
 
-    struct record_key *left_side = malloc(lsize*sizeof(struct record_key));
-    struct record_key *right_side = malloc(rsize*sizeof(struct record_key));
-    for (int i = 0; i < lsize; i++){
-        memcpy(&(left_side[i]), &(array[left+i]), 100);
+    struct record_key *left_side = malloc(lsize * sizeof(struct record_key));
+    struct record_key *right_side = malloc(rsize * sizeof(struct record_key));
+    for (int i = 0; i < lsize; i++)
+    {
+        memcpy(&(left_side[i]), &(array[left + i]), 100);
     }
-    for (int i = mid; i < rsize; i++){
-        memcpy(&(right_side[i]), &(array[mid+i+1]), 100);
+    for (int i = mid; i < rsize; i++)
+    {
+        memcpy(&(right_side[i]), &(array[mid + i + 1]), 100);
     }
 
     // merge two half
     int i = 0;
     int j = 0;
     int k = 0;
-    while(i < lsize && j < rsize){
-    //printf("i %d ; j %d\n", i,j);
-    if (left_side[i].key < right_side[j].key){
-      array[left + k] = left_side[i];
-      i++; 
-    } else{
-      array[left + k] =right_side[j];
-      j++; 
+    while (i < lsize && j < rsize)
+    {
+        // printf("i %d ; j %d\n", i,j);
+        if (left_side[i].key < right_side[j].key)
+        {
+            array[left + k] = left_side[i];
+            i++;
+        }
+        else
+        {
+            array[left + k] = right_side[j];
+            j++;
+        }
+        k++;
     }
-    k++;
-  }
 
     // add remaining elements if possible/needed
-  while (i < lsize){
-    array[left + k] = left_side[i];
-    i++; 
-    k++; 
-  }
-  while (j < rsize){
-    array[left + k] = right_side[j];
-    j++; 
-    k++; 
-  }
+    while (i < lsize)
+    {
+        array[left + k] = left_side[i];
+        i++;
+        k++;
+    }
+    while (j < rsize)
+    {
+        array[left + k] = right_side[j];
+        j++;
+        k++;
+    }
 
-
-  free(left_side);
-  free(right_side);
+    free(left_side);
+    free(right_side);
 }
 
-void mergeSort(struct record_key *array, int left, int right){
+void mergeSort(struct record_key *array, int left, int right)
+{
     int mid = left + (right - left) / 2;
-    if (left < right){
+    if (left < right)
+    {
         mergeSort(array, left, mid);
-        mergeSort(array, mid+1, right);
+        mergeSort(array, mid + 1, right);
         merge(array, left, mid, right);
     }
 }
 
-void mergeAll(struct record_jet *array, int numPartitions, int partition){
-    int num_processes = get_nprocs();
-    int num_entries = (num_records/num_processes);
-    pthread_t threads[numPartitions/2];
-    for (int i = 0; i < numPartitions; i+=2){
-        int left = i * num_entries * partition;
-        int right = ((i + 2) *num_entries * partition);
+void *thread_helper(void *numThreads)
+{
+    int numEntries = (num_records / num_processes);
+    int threads = (long)numThreads;
+
+    int left = threads * numEntries;
+    int right = (threads + 1) * numEntries - 1;
+
+    if (threads == num_processes - 1)
+    {
+        right += remain_work;
     }
+    int mid = left + (right - left) / 2;
+    // mergesort on 1st half and second half then combine everything together
+    if (left < right)
+    {
+        mergeSort(array, left, mid);
+        mergeSort(array, mid + 1, right);
+        merge(array, left, mid, right);
+    }
+    return NULL;
 }
 
+void *thread2_helper(void *args)
+{
+    int *args2 = (int *)args;
+    merge(array, args2[0], args2[1], args2[2]);
+    free(args);
+    return NULL;
+}
+
+
+void mergeAll(struct record_key *array, int numPartitions, int partition)
+{
+    int num_entries = (num_records / num_processes);
+    pthread_t threads[numPartitions / 2];
+    for (int i = 0; i < numPartitions; i += 2)
+    {
+        int left = i * num_entries * partition;
+        int right = ((i + 2) * num_entries * partition);
+        int mid = left + (num_entries * partition) - 1;
+
+        // base case
+        if (right >= num_records)
+        {
+            right = num_records - 1;
+        }
+        if (numPartitions <= 2)
+        {
+            merge(array, left, mid, right);
+        }
+        else
+        {
+            // thread the merge
+            int *args = (int *)malloc(3 * sizeof(int));
+            args[0] = left;
+            args[1] = mid;
+            args[2] = right;
+            int thread_status = pthread_create(&threads[(int)i / 2], NULL, thread2_helper, (void *)args);
+            if (thread_status != 0)
+            {
+                fprintf(stderr, "error\n");
+                exit(0);
+            }
+        }
+    }
+
+    if (numPartitions > 1)
+    {
+        for (int i = 0; i < numPartitions / 2; i++)
+        {
+            pthread_join(threads[i], NULL);
+        }
+    }
+    // recursively merge again
+    int remainingPartitions = numPartitions / 2;
+    if (remainingPartitions >= 1)
+    {
+        mergeAll(array, remainingPartitions, partition * 2);
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -95,7 +180,7 @@ int main(int argc, char **argv)
     }
 
     char *input_file = argv[1];
-   // char *output_file = argv[2];
+    char *output_file = argv[2];
     int numThreads = atoi(argv[3]);
 
     // trying to open input file
@@ -107,7 +192,8 @@ int main(int argc, char **argv)
     }
 
     // check for valid number of threads
-    if (numThreads < 1 || numThreads > get_nprocs()){
+    if (numThreads < 1 || numThreads > get_nprocs())
+    {
         fprintf(stderr, "An error has occurred\n");
     }
 
@@ -117,35 +203,66 @@ int main(int argc, char **argv)
 
     // checking size if input file
     int size = (int)file_s.st_size;
-    if (size <= 0 || size % 100 != 0){
+    if (size <= 0 || size % 100 != 0)
+    {
         fprintf(stderr, "An error has occurred\n");
+        exit(0);
     }
 
     // grab the number of records
-    num_records = size/RECORD;
+    num_records = size / RECORD;
 
     // fill in our array to be sorted
-    array = malloc(num_records*sizeof(struct record_key));
+    array = malloc(num_records * sizeof(struct record_key));
 
-    char* data = mmap(0, size, PROT_READ, MAP_PRIVATE, fd, 0);
-    for(int i = 0; i < num_records; i++){
+    char *data = mmap(0, size, PROT_READ, MAP_PRIVATE, fd, 0);
+    for (int i = 0; i < num_records; i++)
+    {
         // putting memory map data (every 100 bytes) into record_key
-        struct record_key* rec = (struct record_key*)(data + (i * 100));
+        struct record_key *rec = (struct record_key *)(data + (i * 100));
         memcpy(&(array[i]), rec, sizeof(struct record_key));
     }
     fclose(fp);
 
-
-    
-
+    num_processes = get_nprocs();
     // create the thread pool
- //   pthread_t thread[numThreads];
+    remain_work = num_records % num_processes;
+    pthread_t thread[numThreads];
 
     // start the psort
-    // for (int i = 0; i < numThreads; i++)
-    // {
-    //     pthread_create(&thread[i], NULL, thread_helper, NULL);
-    // }
-    
+    for (long i = 0; i < numThreads; i++)
+    {
+        int thread_status = pthread_create(&thread[i], NULL, thread_helper, (void *)i);
+        if (thread_status != 0)
+        {
+            fprintf(stderr, "An error has occurred\n");
+            exit(0);
+        }
+    }
+    for (int i = 0; i < num_processes; i++)
+    {
+        pthread_join(thread[i], NULL);
+    }
+
+    mergeAll(array, num_processes, 1);
+
+    int fptr;
+    fptr = open(output_file, O_WRONLY, O_APPEND);
+    if (fptr == -1)
+    {
+        fprintf(stderr, "An error has occurred\n");
+        exit(0);
+    }
+    for (int i = 0; i < num_records; i++)
+    {
+        int rc = write(fptr, &(array[i]), sizeof(struct record_key));
+        if (rc != 100)
+        {
+            fprintf(stderr, "An error has occurred\n");
+            exit(0);
+        }
+    }
+    fsync(fptr);
+    close(fptr);
     return 0;
 }
